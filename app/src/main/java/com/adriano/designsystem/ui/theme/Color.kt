7969ed518b7d386f8c0b1288e2b64f15718e8f2e
AdrianoCelentano <AdrianoCelentano@users.noreply.github.com>
2025-11-25 16:24:02 +0000

    design: update compose theme

---
+++ app/src/main/java/com/adriano/designsystem/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.adriano.designsystem.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF3A693B)
+val Secondary = Color(0xFF52634F)
+val Tertiary = Color(0xFF39656B)
