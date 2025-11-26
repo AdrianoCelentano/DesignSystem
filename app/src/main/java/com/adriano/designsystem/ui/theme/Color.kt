42971c24f57beab085d3523e9b97d5f6f3b556db
AdrianoCelentano <1528394+AdrianoCelentano@users.noreply.github.com>
2025-11-26 15:20:10 +0000

    design: update compose theme

--- app/src/main/java/com/adriano/designsystem/ui/theme/Color.kt
+++ app/src/main/java/com/adriano/designsystem/ui/theme/Color.kt
@@ -7,3 +7,7 @@ public val Primary: Color = Color(0xFF3A693B)
 public val Secondary: Color = Color(0xFF52634F)
 
 public val Tertiary: Color = Color(0xFF39656B)
+
+public val OnPrimary: Color = Color(0xFFFFFFFF)
+
+public val Error: Color = Color(0xFFBA1A1A)
